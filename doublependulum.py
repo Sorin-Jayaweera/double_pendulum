0de1dc9216a4c0de26bc74755cb982ee5cf09f19
WunderBear <sorin@jayaweera.com>
2025-04-23 20:23:54 -0700

    WORKING

--- doublependulum.py
+++ doublependulum.py
@@ -8,7 +8,6 @@ Created on Sun Apr 20 14:35:43 2025
 @author: wunderbear
 """
 
-# IMPORTS
 
 import numpy as np
 import scipy as sp
@@ -20,15 +19,6 @@ import matplotlib.animation as animation
 from matplotlib.animation import FuncAnimation
 import time
 
-# Parameters of the system
-theta = sym.symbols("θ")
-dtheta = sym.symbols("dθ")
-m1,l1,m2,l2, g = [1,1,1,1,9.8]
-L = sym.symbols("L")
-
-
-upperlim = 20
-numpoints = 40 * upperlim +1
 
 def matStuff(theta1, w1, theta2, w2, m1, l1, m2, l2, g):
     
@@ -38,12 +28,11 @@ def matStuff(theta1, w1, theta2, w2, m1, l1, m2, l2, g):
     ep = m2*l2**2
     
     A = np.matrix([(al, be), (de, ep)])
-
     
-    gamma =  m1*l1*l2*np.sin(theta1-theta2) * (w1-w2) * w2 + g*(m1+m2)*l1*np.sin(theta1) - m2*w1*w2*l1*l2*np.sin(theta1-theta2)
+    gamma = m1*l1*l2*np.sin(theta1-theta2) * (w1-w2) * w2 + g*(m1+m2)*l1*np.sin(theta1) - m2*w1*w2*l1*l2*np.sin(theta1-theta2)
 
     #gamma = m2*w2*l1*l2*np.sin(theta1-theta2)*(w1-w2) - m2*w1*w2*l1*l2*np.sin(theta1 -theta2) +g*(m1+m2)*l1*np.sin(theta1) #m1*l1*l2*w2*np.sin(theta1 - theta2)*(w1-w2) + g*(m1 + m2)*l1*np.sin(theta1) - m2*w1*w2*l1*l2*np.sin(theta1 - theta2)
-    phi =  m2*w1*l1*l2*np.sin(theta1-theta2)*(w1-w2) + g*m2*l2*np.sin(theta2) + m2*w1*w2*l1*l2*np.sin(theta1-theta2) #(m2*l1*l2*np.sin(theta1 - theta2)*(w1-w2))*w1 + g*m2*l2*np.sin(theta2) + m2*theta2*l2**2 + m2
+    phi = m2*w1*l1*l2*np.sin(theta1-theta2)*(w1-w2) + g*m2*l2*np.sin(theta2) + m2*w1*w2*l1*l2*np.sin(theta1-theta2) #(m2*l1*l2*np.sin(theta1 - theta2)*(w1-w2))*w1 + g*m2*l2*np.sin(theta2) + m2*theta2*l2**2 + m2
     v = np.matrix([[gamma], [phi]])
     
     invA = np.matrix.getI(A)
@@ -68,8 +57,15 @@ def derivatives(t, X, m1, l1, m2, l2, g):
     
     return [w1, a1, w2, a2]
 
-theta10 = np.pi/3
-theta20 = 0
+
+
+upperlim = 10
+numpoints = 40 * upperlim +1
+
+m1,l1,m2,l2, g = [1,1,1,1,-9.8]
+
+theta10 = np.pi/2#np.pi/3
+theta20 = np.pi/2
 
 res1 = solve_ivp(derivatives, [0, upperlim], [theta10,0,theta20,0],t_eval=np.linspace(0,upperlim,numpoints),args=(m1,l1,m2,l2,g))
 
@@ -89,7 +85,7 @@ x2 = x1 + l2* np.sin(theta2)
 y2 = y1 - l2*np.cos(theta2)
 
 
-        
+
 fig, ax = plt.subplots()
 
 ax.set_xlabel('T [Samples]')
@@ -104,10 +100,10 @@ def update(i):
     ax.clear()
 
     ax.set_aspect(1)
-    plt.xlim(-5, 5)
-    plt.ylim(-5, 5)
-    #plt.xlim(-l1-l2, l1+l2)
-    #plt.ylim(0, l1+l2)
+    #plt.xlim(-5, 5)
+    #plt.ylim(-5, 5)
+    plt.xlim(-l1-l2, l1+l2)
+    plt.ylim(0, 2*(l1+l2))
     
     ax.scatter(x1[i],y1[i])
     ax.plot([0,x1[i]],[h,y1[i]])
@@ -115,41 +111,7 @@ def update(i):
     ax.plot([x1[i],x2[i]],[y1[i],y2[i]])
 
     
-ani = animation.FuncAnimation(fig=fig, func=update, frames=len(x1), interval=30)
+ani = FuncAnimation(fig=fig, func=update, frames=len(x1), interval=30)
 plt.show()
 
 
-"""
-theta10 = np.pi/2
-theta20 = np.pi/2
-
-res1 = solve_ivp(derivatives, [0, upperlim], [theta10,0,theta20,0],t_eval=np.linspace(0,upperlim,numpoints),args=(m1,l1,m2,l2,g))
-
-plt.close("all")
-t1 = res1.t
-x = l * np.sin(res1.y[0,:])
-y = l-(l * ( np.cos(res1.y[0,:])))
-
-        
-fig, ax = plt.subplots()
-
-ax.set_xlabel('T [Samples]')
-ax.set_ylabel('X')
-
-ax.set_aspect(1)
-ax.scatter(0,l)
-
-
-def update(i):
-    ax.clear()
-
-    ax.set_aspect(1)
-    plt.xlim(-5, 5)
-    plt.ylim(-5, 5)
-    ax.scatter(x[i],y[i])
-    ax.plot([0,x[i]],[l,y[i]])
-
-    
-ani = animation.FuncAnimation(fig=fig, func=update, frames=len(x), interval=30)
-plt.show()
-"""
